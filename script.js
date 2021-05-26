b521611a17dc5236067276b58e32ad7a37cd6ac8
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2021-05-25 21:56:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function introFade() {
+    let intro = document.querySelector('.intro')
+    if (intro.className === "intro") {
+        intro.className += " introPressed";
+    }
+    else {
+        className = "intro"
+    }
+}
