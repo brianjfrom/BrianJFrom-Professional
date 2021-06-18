688d9c44018de4ebbbf4c6e5117a7bedc2df9c86
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2021-06-18 03:59:59 -0600

    update to almost final

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function resNav() {
+    let res = document.querySelector('.topnav')
+    if (res.className === "topnav") {
+        res.className += " dropdown";
+    }
+    else {
+        res.className = "topnav" 
+    }
+}
+
+
+
 function introFade() {
     let intro = document.querySelector('.intro')
     if (intro.className === "intro") {
